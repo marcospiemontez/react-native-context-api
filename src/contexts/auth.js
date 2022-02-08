cc8ede849d095fa7be8d5bf6019782a427a65ff1
Marcos Piemontez <marcos.piemontez1@gmail.com>
2022-02-07 22:42:49 -0300

    ➕ ADD DEP: Testando API

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
 export const AuthContext = createContext({});
 
@@ -8,19 +9,23 @@ function AuthProvider({children}) {
   const navigation = useNavigation()
 
   function signIn(email, password) {
-    if(email !== ''&& password !== '') {
-      setUser({
-        email: email,
-        status: 'active'
+    if(email !== '' && password !== '') {
+      axios.post('http://localhost:3000/authentication/login', {email: email, password: password}).then(res => {
+        console.log(res, 'tetete')
+        setUser({
+          pet: res.data,
+          status: 'active'
+        })
+        navigation.navigate("Home");
       })
-
-      navigation.navigate("Home");
+    } else {
+      alert('Coloca a senha ai cavalo!')
     }
   }
 
 
   return (
-    <AuthContext.Provider value={{ nome: "Marcos Piemontez", signIn, user }}>
+    <AuthContext.Provider value={{ signIn, user }}>
       {children}
     </AuthContext.Provider>
   );
